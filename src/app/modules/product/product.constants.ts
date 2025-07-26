5f730dba7d5c200e76e319a8a2110d7f3207054d
Radiat09 <rzsianboyridoy10@gmail.com>
2025-07-26 12:45:00 +0600

    refactor: remove unused product and user constants files

--- src/app/modules/product/product.constants.ts
+++
@@ -1,3 +0,0 @@
-export const PRODUCT_SEARCHABLE_FIELDS = ["title", "description", "category"];
-
-export const PRODUCT_SORTABLE_FIELDS = ["title", "price", "sellCount", "createdAt", "discountedPrice"];
--- src/app/modules/user/user.constants.ts
+++
@@ -1,5 +0,0 @@
-export const USER_SEARCHABLE_FIELDS = ["name", "email"];
-
-export const USER_FILTERABLE_FIELDS = ["customer"];
-
-export const USER_SORTABLE_FIELDS = ["name", "email", "createdAt"];
